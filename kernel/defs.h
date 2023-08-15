4930eb63d083198c59d84e09c9eaea3f5c18575d
Soiaoder <1424067075@qq.com>
2023-08-16 01:41:50 +0800

    ready to submit my lab

--- kernel/defs.h
+++ kernel/defs.h
@@ -67,6 +67,7 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+void *          ksteal(int cpu);
 
 // log.c
 void            initlog(int, struct superblock*);
